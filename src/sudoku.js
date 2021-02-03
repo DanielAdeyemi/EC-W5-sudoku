f805bbc4452a4334e23c3b82589b4e48cb10ec82
Ashley Porter <porter.ashley13@gmail.com>
2021-02-02 16:10:00 -0800

    CORRECTED COMMIT pass test for checkRow() function with all rows
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>
    Co-authored-by: Sage Sypert <sage.sypert@gmail.com>

--- src/sudoku.js
+++ src/sudoku.js
@@ -21,44 +21,44 @@ export default function Sudoku(array) {
 }
 
 Sudoku.prototype.checkRow = function() {
-  let row0 = this.row0;
-  let row1 = this.row1;
-  let row2 = this.row2;
-  let row3 = this.row3;
-  let row4 = this.row4;
-  let row5 = this.row5;
-  let row6 = this.row6;
-  let row7 = this.row7;
-  let row8 = this.row8;
+  // let row0 = this.row0;
+  // let row1 = this.row1;
+  // let row2 = this.row2;
+  // let row3 = this.row3;
+  // let row4 = this.row4;
+  // let row5 = this.row5;
+  // let row6 = this.row6;
+  // let row7 = this.row7;
+  // let row8 = this.row8;
 
-  let sum0 = 0;
-  let sum1 = 0;
-  let sum2 = 0;
-  let sum3 = 0;
-  let sum4 = 0;
-  let sum5 = 0;
-  let sum6 = 0;
-  let sum7 = 0;
-  let sum8 = 0;
+  // let sum0 = 0;
+  // let sum1 = 0;
+  // let sum2 = 0;
+  // let sum3 = 0;
+  // let sum4 = 0;
+  // let sum5 = 0;
+  // let sum6 = 0;
+  // let sum7 = 0;
+  // let sum8 = 0;
 
-  for (let i = 0; i < 9; i++) {
-    sum0 += row0[i];
-    sum1 += row1[i];
-    sum2 += row2[i];
-    sum3 += row3[i];
-    sum4 += row4[i];
-    sum5 += row5[i];
-    sum6 += row6[i];
-    sum7 += row7[i];
-    sum8 += row8[i];
-  }
+  // for (let i = 0; i < 9; i++) {
+  //   sum0 += row0[i];
+  //   sum1 += row1[i];
+  //   sum2 += row2[i];
+  //   sum3 += row3[i];
+  //   sum4 += row4[i];
+  //   sum5 += row5[i];
+  //   sum6 += row6[i];
+  //   sum7 += row7[i];
+  //   sum8 += row8[i];
+  // }
 
-  let sumAll = sum0 + sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8;
-  if (sumAll === 405) {
-    return "true";
-  } else {
-    return "false";
-  }
+  // let sumAll = sum0 + sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8;
+  // if (sumAll === 405) {
+  //   return "true";
+  // } else {
+  //   return "false";
+  // }
   // for (let i = 0; i < 9; i++) {
   //   let sum = 0;
   //   let row = this.rowi;
